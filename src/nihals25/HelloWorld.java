123216c550b3e04f94c5c7f1f1934033e85ce0c4
nihals25 <nihals25@gmail.com>
2018-10-31 15:44:04 -0500

    HelloWorld and MainApp changes

--- src/nihals25/HelloWorld.java
+++ src/nihals25/HelloWorld.java
@@ -1,4 +1,13 @@
 package nihals25;
 
 public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
