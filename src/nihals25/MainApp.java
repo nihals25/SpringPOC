4e1ea90f05d15a77158bb7e6c7f358f820d3957c
nihals25 <nihals25@gmail.com>
2018-11-01 12:34:46 -0500

    Bean scopes

--- src/nihals25/MainApp.java
+++ src/nihals25/MainApp.java
@@ -6,7 +6,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld obj = (HelloWorld) context.getBean("helloworld");
-        System.out.println(obj.getMessage());
+        HelloWorld obj1 = (HelloWorld) context.getBean("helloworld");
+        obj1.setMessage("First Object");
+        System.out.println(obj1.getMessage());
+        HelloWorld obj2 = (HelloWorld) context.getBean("helloworld1");
+        obj2.setMessage("Hi This is Nihal");
+        System.out.println(obj2.getMessage());
+        HelloWorld obj3 = (HelloWorld) context.getBean("helloworld1");
+        System.out.println(obj3.getMessage());
     }
 }
